c1a3e4d8a2a5b0f233568fe62d7b74256916f533
Vansh <vanshvansh59939@gmail.com>
2026-01-14 18:18:08 +0000

    Day 3: POST requests and validations with Pydantic

--- main.py
+++ main.py
@@ -1,7 +1,26 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
+# -------------------
+# Data Model
+# -------------------
+class User(BaseModel):
+    id: int
+    name: str
+    age: int
+
+# -------------------
+# Routes
+# -------------------
 @app.get("/")
 def root():
     return {"message": "Backend running"}
+
+@app.post("/users")
+def create_user(user: User):
+    return {
+        "status": "user created",
+        "user": user
+    }
